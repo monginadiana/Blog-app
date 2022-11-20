88d2c778b6594fdd82e469ae05a001fc11d4e913
monginadiana <dianahmongina2@gmail.com>
2022-11-20 20:07:45 +0300

    creation of the fetch requests

--- src/App.js
+++ src/App.js
@@ -1,20 +1,39 @@
-
+import { useState, useEffect} from 'react';
 import './App.css';
 import Navbar from './Navbar';
 import Blog from './Blogs';
 import {Routes,Route} from "react-router-dom"
-import Comment from './Comment'
 import SingleBlog from './SingleBlog'
 import NewBlog from './NewBlog'
 
 function App() {
+
+
+  const [blogs, setBlogs] = useState([])
+
+  const url = "http://localhost:9292/blogs"
+
+
+  useEffect(() => {
+    
+
+    fetch(url)
+      .then(res => res.json())
+      .then(data => setBlogs(data))   
+  }, [])
+
+  function addBlog(blog){
+    console.log(blog);
+    setBlogs([...blogs, blog])
+  }
+
   return (
     <div className="App">
   
     <Navbar/>
     <Routes>
     <Route exact path="/" element={<Blog/>}/>
-    <Route exact path="/newBlog" element={<NewBlog/>}/>
+    <Route exact path="/newBlog" element={<NewBlog/>} addBlog={addBlog}/>
     <Route exact path="/blog/:id" element={<SingleBlog/>}/>
     </Routes>
    
