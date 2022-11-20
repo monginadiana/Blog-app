252b191716364c27a8cc4f29cb6eaf8f27f212e3
monginadiana <dianahmongina2@gmail.com>
2022-11-20 15:30:36 +0300

    creation of the navbar.js

---
+++ src/Navbar.js
@@ -0,0 +1,20 @@
+
+export default function Navbar(){
+    return (
+    <div class="topbar">
+      <div class="container navbar">
+            <h1 class="push-left">Mongina's Blog App</h1>
+          
+            <nav role="navigation" class="push-right">
+              <ul>
+                <li><a href="#11">See All Posts</a></li>
+                <li><a href="#1" >Add New Post</a></li>
+              </ul>
+            </nav>
+        
+      </div>
+    </div>
+
+    )
+}
+
